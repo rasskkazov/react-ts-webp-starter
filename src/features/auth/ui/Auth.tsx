5d2ad64768abe0b5387c0dddea9932cd187cf231
Bogdan <brasskazov@gmail.com>
2024-05-16 19:50:48 +0300

    fix: auth btn style

--- src/features/auth/ui/Auth.tsx
+++ src/features/auth/ui/Auth.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/shared/ui/Button";
 import { observer } from "mobx-react";
 import { useAuth } from "../lib/useAuth";
+import * as classes from "./Auth.module.scss";
 export const Auth = observer(() => {
   const { isAuth, isLoading, handleLogIn, handleLogOut } = useAuth();
 
   return (
-    <div className="auth">
+    <div className={classes.auth}>
       {!isAuth && !isLoading && <Button text="Log in" onClick={handleLogIn} />}
       {isLoading && <Button text="Log in" status="loading" />}
       {isAuth && <Button text="Log out" onClick={handleLogOut} />}
--- src/widgets/todoList/ui/TodoList.tsx
+++ src/widgets/todoList/ui/TodoList.tsx
@@ -4,14 +4,14 @@ import { useFetchTodos } from "../lib/useFetchTodos";
 export const TodoList = () => {
   const { data, error, isLoading } = useFetchTodos();
 
-  console.log(authStore.user);
-
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
   return (
     <div className="todo-list">
       {data.map((todo) => (
-        <div className="todos">{todo.title}</div>
+        <div key={todo.id} className="todos">
+          {todo.title}
+        </div>
       ))}
     </div>
   );
